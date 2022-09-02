cc06cebf5c5b509e9047b028819d69c14f18a7d7
Francesco <francesco.209@gmail.com>
2022-09-02 17:46:22 +0200

    Move function to **utils**

--- chunknorris/main.py
+++ chunknorris/main.py
@@ -4,33 +4,9 @@ from typing import Optional
 import typer
 import rich
 
+from utils import open_writer, close_writer, split_extension
+
 app = typer.Typer()
-
-
-def split_extension(filename: str) -> tuple[str, str]:
-    basename = os.path.basename(filename)
-    *parts, extension = basename.split(".")
-    return ".".join(parts), extension
-
-
-def open_writer(
-    basename: str,
-    count: int,
-    extension: str,
-    separator: str = "-",
-    output_dir: str = "",
-    header: str = "",
-) -> TextIOWrapper:
-    writer = open(
-        os.path.join(output_dir, f"{basename}{separator}{count}.{extension}"), "w"
-    )
-    writer.write(header)
-    return writer
-
-
-def close_writer(writer: TextIOWrapper, footer: str = ""):
-    writer.write(footer)
-    writer.close()
 
 
 @app.command()
