cd4e37e8e54f9c59b55479a7b9e67a7e2c6c2bcb
Francesco <francesco.209@gmail.com>
2022-09-02 18:19:25 +0200

    Add filter command

--- chunknorris/main.py
+++ chunknorris/main.py
@@ -4,13 +4,13 @@ from typing import Optional
 import typer
 import rich
 
-from utils import open_writer, close_writer, split_extension
+from chunknorris.utils import open_writer, close_writer, split_extension
 
 app = typer.Typer()
 
 
 @app.command()
-def main(
+def chunk(
     input_file: typer.FileText,
     breaking_line: str,
     occurrences: int,
@@ -22,6 +22,8 @@ def main(
     separator: Optional[str] = typer.Option("-", "--separator", "-s"),
     output_dir: Optional[str] = typer.Option("", "--output-dir", "-od"),
 ):
+    """Splits text file in chunks"""
+
     header_content = header_file.read() if header_file else header
     footer_content = footer_file.read() if footer_file else footer
 
@@ -50,3 +52,53 @@ def main(
     close_writer(writer)
 
     rich.print("[green]Done![/green]")
+
+
+@app.command()
+def filter(
+    input_file: typer.FileText,
+    block_tag: str,
+    tag: str,
+    value: str,
+    separator: Optional[str] = typer.Option("_", "--separator", "-s"),
+    output_dir: Optional[str] = typer.Option("", "--output-dir", "-od"),
+):
+    """Filter XML block"""
+
+    basename, extension = split_extension(input_file.name)
+
+    with open(
+        os.path.join(output_dir, f"{basename}{separator}{value}.{extension}"), "w"
+    ) as fp:
+        buffer: str = ""
+        open_block_tag = f"<{block_tag}>"
+        close_block_tag = f"</{block_tag}>"
+        open_tag = f"<{tag}>"
+        match_value = False
+        in_block = False
+        for line in input_file:
+            if open_block_tag in line:
+                print("Open block", line)
+                buffer = line
+                in_block = True
+            elif close_block_tag in line:
+                print("Close block", line)
+                if match_value:
+                    buffer += line
+                    fp.write(buffer)
+                buffer = ""
+                in_block = False
+            elif open_tag in line:
+                print("Open tag", line)
+                buffer += line
+                match_value = value in line
+            elif not in_block:
+                print("Not in block", line)
+                fp.write(line)
+            else:
+                print("Else", line)
+                buffer += line
+
+
+if __name__ == "__main__":
+    app()
--- chunknorris/utils.py
+++ chunknorris/utils.py
@@ -1,3 +1,7 @@
+from io import TextIOWrapper
+import os
+
+
 def split_extension(filename: str) -> tuple[str, str]:
     basename = os.path.basename(filename)
     *parts, extension = basename.split(".")
